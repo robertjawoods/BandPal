f037ff870e99788e131c6fedacb58412f820aa13
Robert Woods <robertjawoods@gmail.com>
2024-03-16 01:41:09 +0000

    fix prisma adapter

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,17 @@
 /* eslint-disable */
 
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
+
+const connectionString = process.env.DATABASE_URL
+
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool);
+  
+  return new PrismaClient({ adapter })
 }
 
 declare global {
