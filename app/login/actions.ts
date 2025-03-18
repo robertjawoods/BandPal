7a588ab9a3d70b120658e134200adfcfb9a4806f
Robert Woods <robertjawoods@gmail.com>
2025-03-18 17:17:38 +0000

    fixed signup redirect

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,6 +5,18 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/app/lib/supabase/server'
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
 export async function login(formData: FormData) {
   const supabase = await createClient()
 
@@ -15,7 +27,10 @@ export async function login(formData: FormData) {
     password: formData.get('password') as string,
   }
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword({
+    email: data.email,
+    password: data.password,
+  })
 
   console.log(error)
 
@@ -37,7 +52,13 @@ export async function signup(formData: FormData) {
     password: formData.get('password') as string,
   }
 
-  const { error, data } = await supabase.auth.signUp(datas)
+  const { error, data } = await supabase.auth.signUp({
+    email: datas.email,
+    password: datas.password,
+    options: {
+      emailRedirectTo: getURL(),
+    },
+  })
 
   console.log(data)
   console.log(error)
