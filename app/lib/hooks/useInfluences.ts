6fc5d7407c042d341ed87072daa4ebee0ccc9645
waqar <waqzb92@gmail.com>
2025-03-20 23:01:41 +0000

    convert influences component into hook

---
+++ app/lib/hooks/useInfluences.ts
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { useSpotify } from "./useSpotify";
+import debounce from "lodash.debounce";
+
+interface ArtistData {
+  name: string;
+  id: string;
+  genres: string[];
+  image: any[];
+}
+
+interface UseInfluencesProps {
+  userID: string;
+}
+
+type UseInfluencesReturn = {
+  suggestions: ArtistData[];
+  // eslint-disable-next-line no-unused-vars
+  newSearch: (query: string) => void;
+  // eslint-disable-next-line no-unused-vars
+  select: (artist: ArtistData) => void;
+  searchTerm: string;
+};
+
+export const useInfluences = ({
+  userID,
+}: UseInfluencesProps): UseInfluencesReturn => {
+  const { getArtist } = useSpotify();
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState<ArtistData[]>([]);
+
+  const debouncedSearch = debounce(() => {
+    getArtist(searchTerm)
+      .then((data) => {
+        const suggestions = data.artists.items.map((artist: any) => {
+          return {
+            name: artist.name,
+            id: artist.id,
+            genres: artist.genres,
+            image: artist.images,
+          };
+        });
+        console.log("returned suggestions");
+        setSuggestions(suggestions);
+      })
+      .catch(() => {});
+  }, 500);
+
+  const newSearch = (query: string) => {
+    if (query !== searchTerm) {
+      console.log("searching.....");
+      setSearchTerm(query);
+      debouncedSearch();
+    }
+  };
+
+  const select = (artist: ArtistData) => {
+    setSearchTerm(artist.name);
+    setSuggestions([]);
+
+    fetch(`/api/user/profile/${userID}/influence`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: artist.name, id: artist.id }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("response data", data);
+      });
+  };
+
+  return { suggestions, newSearch, select, searchTerm };
+};
--- app/user/edit/[id]/Influences.tsx
+++ app/user/edit/[id]/Influences.tsx
@@ -1,106 +1,52 @@
-'use client'
-
-import { useSpotify } from "@/app/lib/hooks/useSpotify";
-import { useDebounce } from "@/app/lib/hooks/useDebounce";
-import { useEffect, useState, useRef } from "react";
+"use client";
+import { useInfluences } from "@/app/lib/hooks/useInfluences";
 
 interface InfluencesProps {
-    influences: {
+  influences:
+    | {
         name: string;
         id: string;
         profileId: string | null;
-    }[] | undefined;
-    userId: string
-}
-
-interface ArtistData { 
-    name: string;
-    id: string;
-    genres: string[];
-    image: any[];
+      }[]
+    | undefined;
+  userId: string;
 }
 
 export function Influences({ influences, userId }: InfluencesProps) {
-    const { getArtist } = useSpotify();
-
-    console.log('influences', influences);
-
-    const [searchTerm, setSearchTerm] = useState("");
-    const debouncedSearchTerm = useDebounce(searchTerm, 500);
-    const [suggestions, setSuggestions] = useState<ArtistData[]>([]);
-   // const [loading, setLoading] = useState(false);
-    const previousSearchTerm = useRef("");
-
-    useEffect(() => {
-        if (!debouncedSearchTerm || debouncedSearchTerm === previousSearchTerm.current) {
-            return;
-        }
-
-        //setLoading(true);
-        getArtist(debouncedSearchTerm)
-            .then(data => {
-                const suggestions = data.artists.items.map((artist: any) => {
-                    return { 
-                        name: artist.name,
-                        id: artist.id,
-                        genres: artist.genres, 
-                        image: artist.images
-                    }
-                });
-                
-                setSuggestions(suggestions);
-                previousSearchTerm.current = debouncedSearchTerm;
-                //setLoading(false);
-
-                console.log('suggestions', suggestions);
-            })
-            .catch(() => {})//setLoading(false));
-    }, [debouncedSearchTerm, getArtist]);
-
-    const handleSelect = (artist: ArtistData) => {
-        setSearchTerm(artist.name);
-        setSuggestions([]);
-
-        fetch(`/api/user/profile/${userId}/influence`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({name: artist.name, id: artist.id})
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log('response data', data);
-        })
-      };
-
-    return (
-        <div className="relative w-full max-w-sm">
-          <label htmlFor="search" className="block text-sm font-medium text-gray-700">
-            Search
-          </label>
-          <input
-            id="search"
-            type="text"
-            name="search"
-            autoComplete="off"
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          {suggestions.length > 0 && (
-            <ul className="absolute z-10 w-full bg-white shadow-lg border border-gray-300 rounded-md mt-1 max-h-60 overflow-auto">
-              {suggestions.map(artist => (
-                <li
-                  key={artist.id}
-                  onClick={() => handleSelect(artist)}
-                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                >
-                  {artist.name}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      );
+  console.log("influences", influences);
+  const { suggestions, newSearch, select, searchTerm } = useInfluences({
+    userID: userId,
+  });
+  return (
+    <div className="relative w-full max-w-sm">
+      <label
+        htmlFor="search"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Search
+      </label>
+      <input
+        id="search"
+        type="text"
+        name="search"
+        autoComplete="off"
+        value={searchTerm}
+        onChange={(e) => newSearch(e.target.value)}
+        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      />
+      {suggestions.length > 0 && (
+        <ul className="absolute z-10 w-full bg-white shadow-lg border border-gray-300 rounded-md mt-1 max-h-60 overflow-auto">
+          {suggestions.map((artist) => (
+            <li
+              key={artist.id}
+              onClick={() => select(artist)}
+              className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            >
+              {artist.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
