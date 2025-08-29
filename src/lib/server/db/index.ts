f8df0d2af5e01a8e4790131dd8e09e55d72432a0
Robert Woods <robertjawoods@gmail.com>
2025-08-29 02:40:32 +0100

    feat: add Prisma Neon adapter and update schema for driver adapters

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,16 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import { readReplicas } from '@prisma/extension-read-replicas'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaNeon({ connectionString })
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
     .$extends(readReplicas({
         url: [process.env.DATABASE_READ_URL || '']
-     }));
+    }));
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
