1899b14dc960d323bcaf08aaab3fbd8d53faffc0
Robert Woods <robertjawoods@gmail.com>
2025-08-29 02:48:05 +0100

    fix: remove adapter from PrismaClient instantiation for compatibility

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -9,7 +9,7 @@ const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaNeon({ connectionString })
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma || new PrismaClient()
     .$extends(withAccelerate())
     .$extends(readReplicas({
         url: [process.env.DATABASE_READ_URL || '']
